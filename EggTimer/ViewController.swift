12eb556eef5ffb5c642c06d761f160ab51c10bc0
necessities <bear_necessities@icloud.com>
2021-02-18 08:47:52 -0500

    Update function for Timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
 
+    @IBOutlet weak var howDoneQ: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+   
+        print(eggTimes[hardness]!)
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+            if secondsRemaining > 0 {
+                print("\(secondsRemaining) seconds")
+                secondsRemaining -= 1
+            }else {
+                howDoneQ.text = "Done"
+            }
+        
+        }
+        
+        
 
 }
